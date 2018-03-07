d5b30389fb60d56a10aac7174876c409daedb95d
Hajagos <hajagos.martin@gmail.com>
2018-03-07 23:28:33 +0100

    Test camera modifications

--- src/entity/Camera.java
+++ src/entity/Camera.java
@@ -5,21 +5,55 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Camera {
 	private Vector3f position = new Vector3f(0,0,0);
-	private float pitch;
-	private float yaw;
-	private float roll;
+	private float pitch = 1.0f;
+	private float yaw = 1.0f;
+	private float roll = 1.0f;
+
+	private float positionModificationAmount = 0.04f;
+
+	private float angleModificationAmount = 0.6f;
 
 	public Camera(){}
 
-	public void move(){
-		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-			position.z-=0.02f;
+	public void move() {
+		if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
+			position.z -= positionModificationAmount;
 		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-			position.x+=0.02f;
+		if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			position.z += positionModificationAmount;
 		}
-		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-			position.x-=0.02f;
+		if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
+			position.x += positionModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
+			position.x -= positionModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_F)) {
+			position.y += positionModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_G)) {
+			position.y -= positionModificationAmount;
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT)) {
+			yaw -= angleModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) {
+			yaw += angleModificationAmount;
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+			pitch -= angleModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+			pitch += angleModificationAmount;
+		}
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_Q)) {
+			roll -= angleModificationAmount;
+		}
+		if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
+			roll += angleModificationAmount;
 		}
 	}
 
