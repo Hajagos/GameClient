da92a557af435a8b9b3663c7248f8dbd621d8920
Hajagos <hajagos.martin@gmail.com>
2018-03-07 23:27:56 +0100

    Remove hard coded model datas

--- src/test/MainGameLoop.java
+++ src/test/MainGameLoop.java
@@ -40,85 +40,6 @@ public class MainGameLoop {
 		StaticShader shader = new StaticShader();
 		Renderer renderer = new Renderer(shader);
 
-		float[] vertices = {
-				-0.5f,0.5f,0,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,0.5f,0,
-
-				-0.5f,0.5f,1,
-				-0.5f,-0.5f,1,
-				0.5f,-0.5f,1,
-				0.5f,0.5f,1,
-
-				0.5f,0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1,
-				0.5f,0.5f,1,
-
-				-0.5f,0.5f,0,
-				-0.5f,-0.5f,0,
-				-0.5f,-0.5f,1,
-				-0.5f,0.5f,1,
-
-				-0.5f,0.5f,1,
-				-0.5f,0.5f,0,
-				0.5f,0.5f,0,
-				0.5f,0.5f,1,
-
-				-0.5f,-0.5f,1,
-				-0.5f,-0.5f,0,
-				0.5f,-0.5f,0,
-				0.5f,-0.5f,1
-
-		};
-
-		float[] textureCoords = {
-
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0,
-				0,0,
-				0,1,
-				1,1,
-				1,0
-
-
-		};
-
-		int[] indices = {
-				0,1,3,
-				3,1,2,
-				4,5,7,
-				7,5,6,
-				8,9,11,
-				11,9,10,
-				12,13,15,
-				15,13,14,
-				16,17,19,
-				19,17,18,
-				20,21,23,
-				23,21,22
-
-		};
-
 		ObjLoader objLoader = new ObjLoader();
 
 		RawModel model = objLoader.loadObjModel("Porsche_911_GT2", loader);
@@ -134,10 +55,9 @@ public class MainGameLoop {
 			long elapsedTime = 0L;
 
 			//RawModel m = loader.loadToVAO(vertices, indices);
-			modifyVertices(vertices);
 			renderer.prepare();
 
-			entity.increaseRotation(0, 1, 0);
+			//entity.increaseRotation(0, 1, 0);
 			camera.move();
 			renderer.prepare();
 			shader.start();
@@ -168,16 +88,4 @@ public class MainGameLoop {
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
-	/**
-	 * Messing around with vertices...
-	 * @param vertices
-	 */
-	private static void modifyVertices(float[] vertices) {
-		for (int i = 0; i < vertices.length; ++i) {
-			//System.out.println("before: " + vertices[i]);
-			vertices[i] *= 1.0005;
-			//System.out.println("after: " + vertices[i]);
-		}
-	}
-
 }
